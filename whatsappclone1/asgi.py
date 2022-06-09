f22cc774554f0437fe5fd46488daeae9e1652192
Alamin Musa Magaga <55305080+alaminmagaga@users.noreply.github.com>
2022-06-09 10:39:13 +0100

    Update asgi.py

--- whatsappclone1/asgi.py
+++ whatsappclone1/asgi.py
@@ -8,21 +8,26 @@ https://docs.djangoproject.com/en/3.0/howto/deployment/asgi/
 """
 
 import os
-import django
 
 from django.core.asgi import get_asgi_application
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'whatsappclone1.settings')
+application = get_asgi_application()
+
+import os
+import django
+
+
+from channels.routing import ProtocolTypeRouter, URLRouter,get_default_application
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'whatsappclone1.settings')
+django.setup()
+application = get_default_application()
 
 from django.urls import path
 
-from channels.routing import ProtocolTypeRouter, URLRouter
 
 
 from chats.consumers import PersonalChatConsumer
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'whatsappclone1.settings')
-django.setup()
-
-application = get_asgi_application()
 from channels.auth import AuthMiddlewareStack
 
 
